912b736e95c9f36515bbc77dfd1449c1d685b2d4
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2021-08-19 14:05:37 +0800

    update

---
+++ default-settings/files/opkg.rc.d
@@ -0,0 +1,95 @@
+#!/bin/sh
+LOCK=/var/lock/opkgupgrade.lock
+BKOPKG="/etc/backup"
+# 防止重复启动
+[ -f $LOCK ] && exit 1
+touch $LOCK
+mkdir -p $BKOPKG
+if [ ! -f /etc/inited ]; then
+	[ "$(uci get dhcp.@dnsmasq[0].noresolv)" ] && {
+		uci del dhcp.@dnsmasq[0].noresolv
+		uci commit dhcp
+		service dnsmasq reload
+	}
+# sh -c "cat '/usr/share/patch/adblock.patch'  | patch -d '/' -p1 --forward" >/dev/null 2>&1
+fi
+
+if [ ! -f "$BKOPKG/user_installed.opkg" ]; then
+	touch /etc/inited
+fi
+
+function opkgupgrade() {
+	c1=0
+	c2=0
+	c3=0
+	rm -f /tmp/opkgupdate.log
+	while ! curl --retry 3 -m 5 https://op.supes.top >/dev/null 2>&1;do
+		echo "无法连接仓库服务器,请检查网络. $c1" >>/tmp/opkgupdate.log
+		[ $c1 == 120 ] && return || let c1++
+		sleep 5
+	done
+			while :; do
+			opkg update >>/tmp/opkgupdate.log 2>&1
+				if [ "$?" == "0" ]; then
+					def="$(opkg list-upgradable | cut -f 1 -d ' ' | grep -vE 'luci-app-opkg|firewall|base-files|busybox|dnsmasq-full|miniupnpd|luci-mod-network|luci-mod-status|luci-mod-system')"
+					insed="$(cat $BKOPKG/user_installed.opkg)"
+					for ipk in $insed; do
+						uged+="$(opkg list-upgradable | grep $ipk)" 2>/dev/null
+					done
+					upopkg="$uged $def"
+					if [ "$upopkg" != " " ]; then
+							for ipk in $upopkg; do
+								while :; do
+									opkg install --force-overwrite --force-checksum --force-depends $ipk >>/tmp/opkgupdate.log 2>&1
+									if [[ $ipk == luci-app-* ]]; then
+										opkg install --force-overwrite --force-checksum luci-i18n-"$(echo $ipk | cut -d - -f 3-4)"-zh-cn >>/tmp/opkgupdate.log 2>&1
+									fi
+									[[ "$(opkg list-installed | grep $ipk)" ]] && {
+										break
+									}
+									[ $c2 == 3 ] && {
+										echo $ipk >> $BKOPKG/failed.txt
+										sed -i '/$ipk/d' $BKOPKG/user_installed.opkg
+										break
+									} || let c2++
+									sleep 1
+									rm -f /var/lock/opkg.lock
+								done
+							done
+							rm -f /etc/config/*-opkg
+					fi
+					touch /etc/inited
+					[ -f $BKOPKG/failed.txt ] && {
+						for ipk in $(cat $BKOPKG/failed.txt); do
+							opkg install --force-overwrite --force-checksum --force-depends $ipk >>/tmp/opkgupdate.log 2>&1
+							[[ "$(opkg list-installed | grep $ipk)" ]] && {
+								sed -i '/$ipk/d' $BKOPKG/failed.txt
+							}
+						done
+						mv $BKOPKG/failed.txt $BKOPKG/failed_.txt
+					}
+					rm -f /var/lock/opkg.lock
+					break
+				fi
+				[ $c3 == 10 ] && break || let c3++
+			done
+			rm -f /var/lock/opkg.lock
+}
+(
+	if [[ ! -f /etc/inited || -f $BKOPKG/failed.txt ]]; then
+		opkgupgrade || true
+	elif [[ -f /etc/inited && `uci get system.@system[0].autoupgrade_pkg 2>/dev/null || echo "1"` != '0' ]]; then
+		opkgupgrade || true
+	fi
+	rm -f /var/lock/opkg.lock
+	
+	[[ -f "/bin/coremark" && ! -f "/etc/bench.log" ]] && {
+		sleep 5
+		/bin/coremark >/tmp/coremark.log
+		cat /tmp/coremark.log | grep "CoreMark 1.0" | cut -d "/" -f 1 >/etc/bench.log
+		sed -i 's/CoreMark 1.0/(CpuMark/g' /etc/bench.log
+		echo " Scores)" >>/etc/bench.log
+	}
+
+rm -f $LOCK
+) &
--- luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
+++ luci-app-gpsysupgrade/luasrc/model/cbi/gpsysupgrade/sysupgrade.lua
@@ -12,24 +12,25 @@ function get_system_version()
 end
 
 function check_update()
-		needs_update, notice = false, false
-		remote_version = luci.sys.exec("echo -n $(curl -s https://op.supes.top/firmware/" ..model.. "/version.txt)")
+		needs_update, notice,md5 = false, false, false
+		remote_version = luci.sys.exec("curl -s https://op.supes.top/firmware/" ..model.. "/version.txt")
 		updatelogs = luci.sys.exec("curl -s https://op.supes.top/firmware/" ..model.. "/updatelogs.txt")
-		remoteformat = luci.sys.exec("date -d $(echo " ..remote_version.. " | awk -F. '{printf $3\"-\"$1\"-\"$2}') +%s")
-		fnotice = luci.sys.exec("echo -n " ..remote_version.. " | sed -n '/\\.$/p'")
-		dateyr = luci.sys.exec("echo -n " ..remote_version.. " | awk -F. '{printf $1\".\"$2}'")
+		remoteformat = luci.sys.exec("date -d $(echo \"" ..remote_version.. "\" | tr '\r\n' ',' | awk -F, '{printf $1}' | awk -F. '{printf $3\"-\"$1\"-\"$2}') +%s")
+		fnotice = luci.sys.exec("echo \"" ..remote_version.. "\" | tr '\r\n' ',' | awk -F, '{printf $(NF-1)}'")
+		dateyr = luci.sys.exec("echo \"" ..remote_version.. "\" | tr '\r\n' ',' | awk -F. '{printf $1\".\"$2}'")
+		md5 = luci.sys.exec("echo \"" ..remote_version.. "\" | tr '\r\n' ',' | awk -F, '{printf $2}'")
+		remote_version = luci.sys.exec("echo \"" ..remote_version.. "\" | tr '\r\n' ',' | awk -F, '{printf $1}' | awk -F. '{printf $1\".\"$2\".\"$3}'")
 		if remoteformat > sysverformat then
 			needs_update = true
-			if currentTimeStamp > remoteformat or fnotice ~= "" then
-				notice = fnotice
+			if currentTimeStamp > remoteformat or fnotice == "1" then
+				notice = true
 			end
 		end
 end
 
 function to_check()
     if not model or model == "" then model = api.auto_get_model() end
-	system_version = get_system_version()
-	sysverformat = luci.sys.exec("date -d $(echo " ..system_version.. " | awk -F. '{printf $3\"-\"$1\"-\"$2}') +%s")
+	sysverformat = luci.sys.exec("date -d $(echo " ..get_system_version().. " | awk -F. '{printf $3\"-\"$1\"-\"$2}') +%s")
 	currentTimeStamp = luci.sys.exec("expr $(date -d \"$(date '+%Y-%m-%d %H:%M:%S')\" +%s) - 172800")
 	if model == "x86_64" then
 		check_update()
@@ -37,50 +38,43 @@ function to_check()
 			download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-x86-64-generic-squashfs-combined-efi.img.gz"
 		else
 			download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-x86-64-generic-squashfs-combined.img.gz"
+			md5 = ""
 		end
-    elseif model:match(".*K2P.*") then
-		model = "phicomm-k2p"
-		check_update()
-        download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-ramips-mt7621-phicomm_k2p-squashfs-sysupgrade.bin"
-    elseif model:match(".*AC2100.*") then
-		model = "redmi-ac2100"
-		check_update()
-        download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-ramips-mt7621-redmi-ac2100-squashfs-sysupgrade.bin"
     elseif model:match(".*R2S.*") then
 		model = "nanopi-r2s"
 		check_update()
-        download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-rockchip-armv8-nanopi-r2s-squashfs-sysupgrade.img.gz"
+		if fs.access("/overlay/upper") then
+			download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-rockchip-armv8-nanopi-r2s-squashfs-sysupgrade.img.gz"
+		else
+			download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-rockchip-armv8-nanopi-r2s-ext4-sysupgrade.img.gz"
+			md5 = ""
+		end
     elseif model:match(".*R4S.*") then
 		model = "nanopi-r4s"
 		check_update()
-        download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-rockchip-armv8-nanopi-r4s-squashfs-sysupgrade.img.gz"
-    elseif model:match(".*D2.*") then
-		model = "newifi-d2"
+		if fs.access("/overlay/upper") then
+			download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-rockchip-armv8-nanopi-r4s-squashfs-sysupgrade.img.gz"
+		else
+			download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-rockchip-armv8-nanopi-r4s-ext4-sysupgrade.img.gz"
+			md5 = ""
+		end
+    elseif model:match(".*Pi 4 Model B.*") then
+		model = "Rpi-4B"
 		check_update()
-        download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-ramips-mt7621-newifi-d2-squashfs-sysupgrade.bin"
-    elseif model:match(".*XY-C5.*") then
-		model = "XY-C5"
-		check_update()
-		if remoteformat > sysverformat and currentTimeStamp > remoteformat then needs_update = true else needs_update = false end
-        download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-ramips-mt7621-xy-c5-squashfs-sysupgrade.bin"
-    elseif model:match(".*Mi Router 3 Pro.*") then
-		model = "xiaomi-r3p"
-		check_update()
-		if remoteformat > sysverformat and currentTimeStamp > remoteformat then needs_update = true else needs_update = false end
-        download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-ramips-mt7621-xiaomi_mir3p-squashfs-sysupgrade.bin"
-	else
+		download_url = "https://op.supes.top/firmware/" ..model.. "/" ..dateyr.. "-openwrt-bcm27xx-bcm2711-rpi-4-squashfs-sysupgrade.img.gz"
+    else
 		local needs_update = false
 		return {
             code = 1,
             error = i18n.translate("Can't determine MODEL, or MODEL not supported.")
 			}
-	end
+    end
 	
 
     if needs_update and not download_url then
         return {
             code = 1,
-            now_version = system_version,
+            now_version = get_system_version(),
             version = remote_version,
             error = i18n.translate(
                 "New version found, but failed to get new version download url.")
@@ -91,14 +85,15 @@ function to_check()
         code = 0,
         update = needs_update,
 		notice = notice,
-        now_version = system_version,
+        now_version = get_system_version(),
         version = remote_version,
+		md5 = md5,
 	logs = updatelogs,
         url = download_url
     }
 end
 
-function to_download(url)
+function to_download(url,md5)
     if not url or url == "" then
         return {code = 1, error = i18n.translate("Download url is required.")}
     end
@@ -108,6 +103,16 @@ function to_download(url)
     local tmp_file = util.trim(util.exec("mktemp -u -t firmware_download.XXXXXX"))
 
     local result = api.exec(api.curl, {api._unpack(api.curl_args), "-o", tmp_file, url}, nil, api.command_timeout) == 0
+
+	local md5local = sys.exec("echo -n $(md5sum " .. tmp_file .. " | awk '{print $1}')")
+	
+	if md5 ~= "" and md5local ~= md5 then
+		api.exec("/bin/rm", {"-f", tmp_file})
+		return {
+            code = 1,
+            error = i18n.translatef("Md5 check failed: %s", url)
+        }
+	end
 
     if not result then
         api.exec("/bin/rm", {"-f", tmp_file})
@@ -127,6 +132,9 @@ function to_flash(file,retain)
 if not retain or retain == "" then
 	local result = api.exec("/sbin/sysupgrade", {file}, nil, api.command_timeout) == 0
 else
+	if retain:match(".*-k.*") then
+		luci.sys.exec("echo -e /etc/backup/user_installed.opkg>/lib/upgrade/keep.d/luci-app-gpsysupgrade")
+	end
 	local result = api.exec("/sbin/sysupgrade", {retain, file}, nil, api.command_timeout) == 0
 end
 
